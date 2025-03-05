a454c623432da5c0dd13236573dac7d03bee4eda
srabhiram <sriramoji.abhi@gmail.com>
2025-03-05 22:00:40 +0530

    Added contact component

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Contact from '@/components/Contact'
+import React from 'react'
+
+function ContactPage() {
+  return (
+    <section>
+      <Contact />
+    </section>
+  )
+}
+
+export default ContactPage
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { EnvelopeSimple } from "@phosphor-icons/react/dist/ssr";
+import React from "react";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <h2 className="text-xl tracking-tight font-semibold mb-4">Contact</h2>
+      <p>Feel free to reach me out</p>
+      <span className="inline-flex items-center gap-1 mt-4">
+        <EnvelopeSimple size={22}/>
+        <a
+          href="mailto:sriramoji.abhiram@gmail.com"
+          className="underline text-white/85 hover:text-white"
+        >
+          sriramoji.abhiram@gmail.com
+        </a>
+      </span>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
