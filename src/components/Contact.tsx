0c53733aff2f63924a89f4e93ef1d63a0a25af51
srabhiram <sriramoji.abhi@gmail.com>
2025-03-06 22:07:17 +0530

    bug fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Contact() {
   return (
     <div>
-      <h2 className="text-xl tracking-tight font-semibold mb-4">Contact</h2>
+      <h2 className="text-xl tracking-tight font-semibold mb-4">Contact 📧</h2>
       <p>Feel free to reach me out</p>
       <span className="inline-flex items-center gap-1 mt-4">
         <EnvelopeSimple size={22}/>
